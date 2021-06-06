70871912902225485b538a741e46fb2060243cd2
blackymax <maxmin3007@gmail.com>
2021-06-06 21:45:28 +0300

    feat: add exit with unhandledRejection/uncaughtException

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -9,12 +9,13 @@ export const handleErrors = (): void => {
         fs.appendFileSync(__dirname + '/../../logs/error_log.txt', errorLogInfo);
         console.error(errorLogInfo);
         process.stdout.write(errorLogInfo);
-
+        process.exit(1);
     })
     process.on('unhandledRejection', (err: Error) => {
         const currTime = new Date();
         const errorLogInfo = `Date: ${currTime.toUTCString()}; status: 500; error: ${err.message}; error-type: unhandledRejection${newLineChar}`
         fs.appendFileSync(__dirname + '/../../logs/error_log.txt', errorLogInfo);
         process.stdout.write(errorLogInfo);
+        process.exit(1);
     })
 }
