70688bb68b3d0a98f541015528f0d21815c9aa30
Maxim Chernov <maxmin3007@gmail.com>
2021-06-17 21:09:32 +0300

    Logging and error hadling (#3)
    * feat: add error-handling and logging

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,19 @@ import * as userRouter from './resources/users/user.router';
 import * as boardRouter from './resources/board/board.router';
 import * as taskRouter from './resources/task/task.router';
 
+import { logging } from './middleware/logging-handler';
+import { handleErrors } from './middleware/error-handler';
+import { errorLogger } from './middleware/error-logging';
+
 export const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
-
 app.use(express.json());
+handleErrors();
+app.use(logging)
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/users', userRouter.router);
 app.use('/boards', boardRouter.router);
 app.use('/boards/:boardId/tasks', taskRouter.router);
+app.use(errorLogger)
+
---
+++ src/middleware/error-handler.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+
+const newLineChar = process.platform === 'win32' ? '\r\n' : '\n';
+
+export const handleErrors = (): void => {
+    process.on('uncaughtException', (err:Error) => {
+        const currTime = new Date();
+        const errorLogInfo = `Date: ${currTime.toUTCString()}; status: 500; error: ${err.name} ${err.message}; error-type: uncaughtException${newLineChar} `
+        fs.appendFileSync(__dirname + '/../../logs/error_log.txt', errorLogInfo);
+        console.error(errorLogInfo);
+        process.stdout.write(errorLogInfo);
+        process.exit(1);
+    })
+    process.on('unhandledRejection', (err: Error) => {
+        const currTime = new Date();
+        const errorLogInfo = `Date: ${currTime.toUTCString()}; status: 500; error: ${err.message}; error-type: unhandledRejection${newLineChar}`
+        fs.appendFileSync(__dirname + '/../../logs/error_log.txt', errorLogInfo);
+        process.stdout.write(errorLogInfo);
+        process.exit(1);
+    })
+}
