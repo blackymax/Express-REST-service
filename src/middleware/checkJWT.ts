504673a74e247d5de84ec98260e391a3c11a06b4
blackymax <maxmin3007@gmail.com>
2021-06-29 10:49:04 +0300

    fix: token check

--- src/middleware/checkJWT.ts
+++ src/middleware/checkJWT.ts
@@ -5,21 +5,32 @@ import { env } from '../common/config';
 const urls = ['/doc', '/', '/login'];
 const JWT_SECRET_KEY = env.JWT_SECRET_KEY || '';
 
-function checkTokenIsCorrect(req: Request, res: Response, next: NextFunction): Response|void {
-    if (urls.includes(req.url)) {
-        return next();
-    } else {
-        const head = req.headers.authorization;
-        if (head) {
-            const [type, token] = head.split(' ');
-            if (type !== 'Bearer' || !token) {
-                return res.status(401).send({message: 'Wrong auth scheme'});
-            }
-            jwt.verify(token, JWT_SECRET_KEY);
-            return next();
+function checkTokenIsCorrect(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Response | void {
+  if (urls.includes(req.url)) {
+    return next();
+  } else {
+    try {
+      const head = req.headers.authorization;
+      if (head) {
+        const [type, token] = head.split(' ');
+        if (!token) {
+          return res.status(401).send({ message: 'Wrong token' });
+        } else if (type !== 'Bearer') {
+          return res.status(401).send({ message: 'Wrong auth scheme' });
+        } else {
+          jwt.verify(token, JWT_SECRET_KEY);
+          return next();
         }
-        return res.status(401).send({message: "Not authorized"});
+      }
+      return res.status(401).send({ message: 'Not authorized' });
+    } catch (err) {
+      return res.status(401).send({ message: 'Wrong token' });
     }
+  }
 }
 
-export { checkTokenIsCorrect }
+export { checkTokenIsCorrect };
